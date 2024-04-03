fa1c0374847c7d96c9fd336fa6abb8905eee1560
Manasi-Lavekar <manasilavekar1@gmail.com>
2024-04-03 16:48:27 +0530

    Updated background

--- tbhs/src/App.js
+++ tbhs/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import Card from "../src/Components/Card";
+// import Card from "../src/Components/Card";
+import Signup from './pages/signup';
 
 function App() {
   return (
     <Routes>
-      <Route path="/card" element={<Card/>}/>
+      <Route path="/signup" element={<Signup/>}/>
     </Routes>
   );
 }
