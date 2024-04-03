b2f96a33bb43e84a353d671764ddc4e90c668d22
sid040703 <siddhiove0407@gmail.com>
2024-04-03 16:51:44 +0530

    updated

--- tbhs/src/App.js
+++ tbhs/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Card from "../src/Components/Card";
-
+import Home from "../src/Pages/Home"
 function App() {
   return (
     <Routes>
+      <Route index element={<Home/>}/>
       <Route path="/card" element={<Card/>}/>
     </Routes>
   );
---
+++ tbhs/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+function Navbar()
+{
+    return(
+        <>
+        <NavLink to='/'>
+            Home
+        </NavLink>
+        </>
+    )
+}
+export default Navbar;
